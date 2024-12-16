7970d4fea17ee4f6fd099ec116543db6dddca381
kyiithelord <thetzinkyaw9727@gmail.com>
2024-12-16 16:39:10 +0630

    DevPlayZone

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+namespace ClassTeams
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Number of students
+      int students = 18;
+      
+
+
+      // Number of students in a group
+      int groupSize = 3;
+
+
+      // Does groupSize go evenly into students?
+      Console.WriteLine(students % groupSize);
+    }
+  }
+}
