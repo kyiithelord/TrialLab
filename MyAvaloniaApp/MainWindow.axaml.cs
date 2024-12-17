51d6dfa537e0664ab38ce642a422fe6a9655aed7
kyiithelord <thetzinkyaw9727@gmail.com>
2024-12-17 11:20:11 +0630

    update

--- MyAvaloniaApp/MainWindow.axaml.cs
+++ MyAvaloniaApp/MainWindow.axaml.cs
@@ -1,11 +1,40 @@
+// using Avalonia.Controls;
+
+// namespace MyAvaloniaApp;
+
+// public partial class MainWindow : Window
+// {
+//     public MainWindow()
+//     {
+//         InitializeComponent();
+//     }
+// }
+
+
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
 
-namespace MyAvaloniaApp;
-
-public partial class MainWindow : Window
+namespace MyAvaloniaApp
 {
-    public MainWindow()
+    public class MainWindow : Window
     {
-        InitializeComponent();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.AttachDevTools();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        // Event handler for button click
+        private void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            var messageText = this.FindControl<TextBlock>("messageText");
+            messageText.Text = "Button Clicked!";
+        }
     }
 }
