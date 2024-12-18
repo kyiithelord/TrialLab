2c390c56cf47f9067b72dd1ac169067d90921d4d
kyiithelord <thetzinkyaw9727@gmail.com>
2024-12-18 15:40:05 +0630

    method

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f821d68b769459b82958bd07d555922f8dcfbe0c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6c1730f35ffaebe2c1dfbdc211c7dc74e727f3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
